b4ef29af3b2c2cc2df40485642ef8e8161802412
ashwin pc <ashwinpc@amazon.com>
2026-08-28 05:22:04 +0000

    Add license headers to disposition tests
    Signed-off-by: ashwin pc <ashwinpc@amazon.com>

--- tests/unit/services/connectors/contextDisposition.test.ts
+++ tests/unit/services/connectors/contextDisposition.test.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { agentPromptContext } from '@/services/connectors/types';
 
 describe('agentPromptContext', () => {
